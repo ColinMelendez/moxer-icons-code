cec83efdd16a84de67441f077f619606167a5725
Colin <cwrmcolin@gmail.com>
2025-01-14 17:18:03 -0800

    merged change

--- src/declarations/fileNames.ts
+++ src/declarations/fileNames.ts
@@ -159,6 +159,7 @@ export const fileNames = {
 	'eslint.config.cjs': '_file_eslint',
 	'eslint.config.ts': '_file_eslint',
 	'eslint.config.mts': '_file_eslint',
+	'eslint.config.cts': '_file_eslint',
 	// jenkinsfile: '_file_jenkins',
 	// 'gatsby.config.js': '_file_gatsby',
 	// 'firebase.json': '_file_firebase',
